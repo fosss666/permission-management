be978a01f3b063dd1c3a8384e2540ca0f89fdbb1
fo <1745179058@qq.com>
2022-11-01 19:59:14 +0800

    逻辑删除

--- service-system/src/main/java/com/fosss/system/controller/SysRoleController.java
+++ service-system/src/main/java/com/fosss/system/controller/SysRoleController.java
@@ -3,9 +3,7 @@ package com.fosss.system.controller;
 import com.fosss.model.system.SysRole;
 import com.fosss.system.service.SysRoleService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,9 +13,20 @@ public class SysRoleController {
     @Autowired
     private SysRoleService sysRoleService;
 
+    /**
+     * 查询所有角色
+     */
     @GetMapping("/findAll")
     public List<SysRole> findAll(){
         return sysRoleService.list();
+    }
+
+    /**
+     * 逻辑删除
+     */
+    @DeleteMapping("/{id}")
+    public boolean delete(@PathVariable String id){
+        return sysRoleService.removeById(id);
     }
 }
 
