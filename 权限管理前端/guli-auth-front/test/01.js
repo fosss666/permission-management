7ab8a09a8e4bd2b445dcb64244b21c76bbf21b67
fo <1745179058@qq.com>
2022-11-02 21:38:46 +0800

    test

---
+++ 权限管理前端/guli-auth-front/test/01.js
@@ -0,0 +1,21 @@
+/*
+export function list(){
+    console.log("list")
+}
+export function add(a,b){
+    return a+b;
+}*/
+export default {
+    list(){
+        console.log("list")
+    },
+    add(a,b){
+        return a+b;
+    }
+}
+
+
+
+
+
+
---
+++ 权限管理前端/guli-auth-front/test/02.js
@@ -0,0 +1,5 @@
+//导入01.js
+import f1 from "./01.js"
+f1.list()
+let result=f1.add(2,2)
+console.log(result)
