841afac7a9b76865aaf68e5aaef4728f03a0607e
fo <1745179058@qq.com>
2022-11-03 23:18:42 +0800

    页面显示数据

--- 权限管理前端/guli-auth-front/fosss-system-auth/src/views/system/sysRole/list.vue
+++ 权限管理前端/guli-auth-front/fosss-system-auth/src/views/system/sysRole/list.vue
@@ -1,6 +1,43 @@
 <template>
+
   <div class="app-container">
-    角色管理
+    <!-- 表格 -->
+    <el-table
+      v-loading="listLoading"
+      :data="roleList"
+      stripe
+      border
+      style="width: 100%;margin-top: 10px;">
+
+      <el-table-column
+        label="序号"
+        width="70"
+        align="center">
+        <template slot-scope="scope">
+          {{ (page - 1) * limit + scope.$index + 1 }}
+        </template>
+      </el-table-column>
+
+      <el-table-column prop="roleName" label="角色名称" />
+      <el-table-column prop="roleCode" label="角色编码" />
+      <el-table-column prop="createTime" label="创建时间" width="160"/>
+      <el-table-column label="操作" width="200" align="center">
+        <template slot-scope="scope">
+          <el-button type="primary" icon="el-icon-edit" size="mini" @click="edit(scope.row.id)" title="修改"/>
+          <el-button type="danger" icon="el-icon-delete" size="mini" @click="removeDataById(scope.row.id)" title="删除"/>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <!-- 分页组件 -->
+    <el-pagination
+      :current-page="page"
+      :total="total"
+      :page-size="limit"
+      style="padding: 30px 0; text-align: center;"
+      layout="total, prev, pager, next, jumper"
+      @current-change="fetchData"
+    />
   </div>
 </template>
 
