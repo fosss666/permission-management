09eb58a01e43370ea3e2be9cd8fdf22bfe7f55c3
fo <1745179058@qq.com>
2022-11-05 15:44:39 +0800

    修改用户状态前端

--- 权限管理前端/guli-auth-front/fosss-system-auth/src/api/system/user.js
+++ 权限管理前端/guli-auth-front/fosss-system-auth/src/api/system/user.js
@@ -1,53 +1,60 @@
 import request from '@/utils/request'
 
-const api_name='/admin/system/sysUser'
+const api_name = '/admin/system/sysUser'
 
 export default {
 
   //列表功能
-  getPageList(page,limit,searchObj){
+  getPageList(page, limit, searchObj) {
     return request({
-      url:`${api_name}/${page}/${limit}`,
-      method:'get',
-      params:searchObj
+      url: `${api_name}/${page}/${limit}`,
+      method: 'get',
+      params: searchObj
     })
   },
   //根据id删除功能
-  removeDataById(id){
+  removeDataById(id) {
     return request({
-      url:`${api_name}/${id}`,
-      method:'delete'
+      url: `${api_name}/${id}`,
+      method: 'delete'
     })
   },
   //添加
-  addUser(sysUser){
+  addUser(sysUser) {
     return request({
-      url:`${api_name}`,
-      method:'post',
-      data:sysUser
+      url: `${api_name}`,
+      method: 'post',
+      data: sysUser
     })
   },
   //根据id查询角色信息
-  getUserById(id){
+  getUserById(id) {
     return request({
-      url:`${api_name}/${id}`,
-      method:'get'
+      url: `${api_name}/${id}`,
+      method: 'get'
     })
   },
   //修改角色
-  updateUser(sysUser){
+  updateUser(sysUser) {
     return request({
-      url:`${api_name}`,
-      method:'put',
-      data:sysUser
+      url: `${api_name}`,
+      method: 'put',
+      data: sysUser
     })
   },
   //批量删除
-  removeUsers(ids){
+  removeUsers(ids) {
     return request({
-      url:`${api_name}`,
-      method:'delete',
-      data:ids
+      url: `${api_name}`,
+      method: 'delete',
+      data: ids
+    })
+  },
+  //修改用户状态
+  updateStatus(id, status) {
+    return request({
+      url: `${api_name}/${id}/${status}`,
+      method: 'put'
     })
   }
 }
