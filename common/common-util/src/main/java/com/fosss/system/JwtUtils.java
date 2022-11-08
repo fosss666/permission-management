421d6eabca7bad90ffa9d187ca62417801dd06a5
fo <1745179058@qq.com>
2022-11-08 20:54:02 +0800

    添加jwt工具类

---
+++ common/common-util/src/main/java/com/fosss/system/JwtUtils.java
@@ -0,0 +1,95 @@
+package com.fosss.system;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.util.StringUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+
+public class JwtUtils {
+
+    public static final long EXPIRE = 1000 * 60 * 60 * 24;//有效时间
+    public static final String APP_SECRET = "ukc8BDbRigUDaY6pZFfWus2jZWLPHO";//密钥
+
+    public static String getJwtToken(String id, String nickname){
+
+        String JwtToken = Jwts.builder()
+				.setIssuedAt(new Date())//签发日期
+                .setHeaderParam("typ", "JWT")
+                .setHeaderParam("alg", "HS256")
+//                .setSubject("guli-user")//主题
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRE))//过期时间
+                .claim("id", id)
+                .claim("nickname", nickname)
+                .signWith(SignatureAlgorithm.HS256, APP_SECRET)
+                .compact();
+
+        return JwtToken;
+    }
+
+    /**
+     * 判断token是否存在与有效
+     * @param jwtToken
+     * @return
+     */
+    public static boolean checkToken(String jwtToken) {
+        if(StringUtils.isEmpty(jwtToken)) return false;
+        try {
+            Jwts.parser().setSigningKey(APP_SECRET).parseClaimsJws(jwtToken);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * 判断token是否存在与有效
+     * @param request
+     * @return
+     */
+    public static boolean checkToken(HttpServletRequest request) {
+        try {
+            String jwtToken = request.getHeader("token");
+            if(StringUtils.isEmpty(jwtToken)) return false;
+            Jwts.parser().setSigningKey(APP_SECRET).parseClaimsJws(jwtToken);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * 根据token获取id
+     */
+    public static String getUserId(String jwtToken) {
+        if(StringUtils.isEmpty(jwtToken)) return "";
+        Jws<Claims> claimsJws = Jwts.parser().setSigningKey(APP_SECRET).parseClaimsJws(jwtToken);
+        Claims claims = claimsJws.getBody();
+        return (String)claims.get("id");
+    }
+
+    /**
+     * 根据token获取name
+     */
+    public static String getUserName(String jwtToken) {
+        if(StringUtils.isEmpty(jwtToken)) return "";
+        Jws<Claims> claimsJws = Jwts.parser().setSigningKey(APP_SECRET).parseClaimsJws(jwtToken);
+        Claims claims = claimsJws.getBody();
+        return (String)claims.get("nickname");
+    }
+
+//    public static void main(String[] args) {
+//        String token = JwtUtils.getJwtToken("1001", "admin");
+//        System.out.println("token = " + token);
+//        String userId = JwtUtils.getUserId(token);
+//        System.out.println("userId = " + userId);
+//        String userName = JwtUtils.getUserName(token);
+//        System.out.println("userName = " + userName);
+//    }
+}
