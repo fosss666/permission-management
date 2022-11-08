73c4150a2d725c1563657a51fbc2e3d0e123bc18
fo <1745179058@qq.com>
2022-11-08 20:52:46 +0800

    给角色分配菜单前端页面

--- 权限管理前端/guli-auth-front/fosss-system-auth/src/api/system/role.js
+++ 权限管理前端/guli-auth-front/fosss-system-auth/src/api/system/role.js
@@ -1,53 +1,70 @@
 import request from '@/utils/request'
 
-const api_name='/admin/system/sysRole'
+const api_name = '/admin/system/sysRole'
 
 export default {
 
   //列表功能
-  getPageList(page,limit,searchObj){
+  getPageList(page, limit, searchObj) {
     return request({
-      url:`${api_name}/${page}/${limit}`,
-      method:'get',
-      params:searchObj
+      url: `${api_name}/${page}/${limit}`,
+      method: 'get',
+      params: searchObj
     })
   },
   //根据id删除功能
-  removeDataById(id){
+  removeDataById(id) {
     return request({
-      url:`${api_name}/${id}`,
-      method:'delete'
+      url: `${api_name}/${id}`,
+      method: 'delete'
     })
   },
   //添加
-  addRole(sysRole){
+  addRole(sysRole) {
     return request({
-      url:`${api_name}`,
-      method:'post',
-      data:sysRole
+      url: `${api_name}`,
+      method: 'post',
+      data: sysRole
     })
   },
   //根据id查询角色信息
-  getRoleById(id){
+  getRoleById(id) {
     return request({
-      url:`${api_name}/${id}`,
-      method:'get'
+      url: `${api_name}/${id}`,
+      method: 'get'
     })
   },
   //修改角色
-  updateRole(sysRole){
+  updateRole(sysRole) {
     return request({
-      url:`${api_name}`,
-      method:'put',
-      data:sysRole
+      url: `${api_name}`,
+      method: 'put',
+      data: sysRole
     })
   },
   //批量删除
-  removeRoles(ids){
+  removeRoles(ids) {
     return request({
-      url:`${api_name}`,
-      method:'delete',
-      data:ids
+      url: `${api_name}`,
+      method: 'delete',
+      data: ids
+    })
+  },
+
+  //查看某个角色的权限列表
+  toAssign(roleId) {
+    return request({
+      url: `${api_name}/doAssign/${roleId}`,
+      method: 'get'
+    })
+  },
+
+  //给某个角色授权
+  doAssign(assignMenuVo) {
+    return request({
+      url: `${api_name}/doAssign`,
+      method: "post",
+      data: assignMenuVo
     })
   }
 }
--- 权限管理前端/guli-auth-front/fosss-system-auth/src/views/system/sysRole/list.vue
+++ 权限管理前端/guli-auth-front/fosss-system-auth/src/views/system/sysRole/list.vue
@@ -56,6 +56,7 @@
           <el-button type="primary" icon="el-icon-edit" size="mini" @click="edit(scope.row.id)" title="修改"/>
           <el-button type="danger" icon="el-icon-delete" size="mini" @click="removeDataById(scope.row.id)"
                      title="删除"/>
+          <el-button type="warning" icon="el-icon-baseball" size="mini" @click="showAssignAuth(scope.row)" title="分配权限"/>
         </template>
       </el-table-column>
     </el-table>
@@ -110,6 +111,10 @@ export default {
     this.getPageList()
   },
   methods: {
+    //跳转到角色分配路由
+    showAssignAuth(row) {
+      this.$router.push('/system/assignAuth?id='+row.id+'&roleName='+row.roleName);
+    },
     //被选中的复选框
     handleSelectionChange(selection){
       // console.log(selection)
