a3c2d0b49b5efe2de6a6620ee353af232103b70e
fo <1745179058@qq.com>
2022-11-12 16:33:00 +0800

    spring security 1.51改bug

--- service-system/src/main/java/com/fosss/system/service/impl/UserDetailsServiceImpl.java
+++ service-system/src/main/java/com/fosss/system/service/impl/UserDetailsServiceImpl.java
@@ -7,12 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import java.util.Collections;
 
 /**
  * 自定义获取用户信息
  */
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService {
     @Autowired
     private SysUserService sysUserService;
